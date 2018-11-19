804827e607c9b4653c5b770a0ddd5be4b3b8cb5c
SharonFei <43115675+SharonFei@users.noreply.github.com>
2018-11-19 22:19:19 +0800

    Update get_email.py

--- python_practice/get_email.py
+++ python_practice/get_email.py
@@ -13,7 +13,7 @@ pop3_server = input('POP3 server: ')
 server = poplib.POP3(pop3_server)
 # 可以打开或关闭调试信息:
 server.set_debuglevel(1)
-# 可选:打印POP3服务器的欢迎文字:
+# 获取POP3服务器的欢迎文字:
 print(server.getwelcome().decode('utf-8'))
 
 # 身份认证:
@@ -22,7 +22,7 @@ server.pass_(password)
 
 # stat()返回邮件数量和占用空间:
 print('Messages: %s. Size: %s' % server.stat())
-# list()返回所有邮件的编号:
+# list()获取服务器上的邮件列表:
 resp, mails, octets = server.list()
 # 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
 print(mails)
