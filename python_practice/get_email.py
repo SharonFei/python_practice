dd2c6200e579fdb7942fa313d634c8844cbce5a4
SharonFei <43115675+SharonFei@users.noreply.github.com>
2018-11-20 22:56:06 +0800

    Update get_email.py

--- python_practice/get_email.py
+++ python_practice/get_email.py
@@ -1,37 +1,34 @@
-# !/usr/bin/python2
-# -*- coding: UTF-8 -*-
 import poplib
+import getpass
 from email.parser import Parser
 from email.header import decode_header
 from email.utils import parseaddr
+
 
 # 输入邮件地址, 口令和POP3服务器地址:
 email = input('Email: ')
 password = input('Password: ')
 pop3_server = input('POP3 server: ')
 
+
 # 连接到POP3服务器:
 server = poplib.POP3(pop3_server)
 # 可以打开或关闭调试信息:
 server.set_debuglevel(1)
-# 获取POP3服务器的欢迎文字:
+# 获取POP3服务器的欢迎信息:
 print(server.getwelcome().decode('utf-8'))
 
 # 身份认证:
-try:
-        server.user(email)
-        server.pass_(password)
-except poplib.error_proto as e:
-        print("Login failed:", e)
-
+server.user(email)
+server.pass_(password)
 
 # stat()返回邮件数量和占用空间:
-status = server.stat()
-print("Mailbox has %d messages for a total of %d bytes" % (status[0], status[1]))
+print('Messages: %s. Size: %s' % server.stat())
 # list()获取服务器上的邮件列表:
 resp, mails, octets = server.list()
 # 可以查看返回的列表类似[b'1 82923', b'2 2184', ...]
 print(mails)
+
 
 # 获取最新一封邮件, 注意索引号从1开始:
 index = len(mails)
@@ -46,7 +43,7 @@ msg = Parser().parsestr(msg_content)
 # 可以根据邮件索引号直接从服务器删除邮件:
 # server.dele(index)
 # 关闭连接:
-#server.quit()
+server.quit()
 
 
 def decode_str(s):
@@ -68,16 +65,24 @@ def guess_charset(msg):
 
 def print_info(msg, indent=0):
     if indent == 0:
-        for header in ['From', 'To', 'Subject']:
+        for header in ['Subject','From', 'To', 'Cc']:
             value = msg.get(header, '')
             if value:
                 if header == 'Subject':
                     value = decode_str(value)
-                else:
+                if header == 'From':
                     hdr, addr = parseaddr(value)
                     name = decode_str(hdr)
                     value = u'%s <%s>' % (name, addr)
-            print('%s%s: %s' % ('  ' * indent, header, value))
+                if header == 'To'or header == 'Cc':
+                    L = value.split(',')
+                    value = []
+                    for x in L:
+                        hdr, addr = parseaddr(x)
+                        name = decode_str(hdr)
+                        x = u'%s <%s>' % (name, addr)
+                        value.append(x)
+                print('%s%s' % (header, value))
     if (msg.is_multipart()):
         parts = msg.get_payload()
         for n, part in enumerate(parts):
@@ -86,7 +91,7 @@ def print_info(msg, indent=0):
             print_info(part, indent + 1)
     else:
         content_type = msg.get_content_type()
-        if content_type == 'text/plain' or content_type == 'text/html':
+        if content_type == 'text/plain' or content_type=='text/html':
             content = msg.get_payload(decode=True)
             charset = guess_charset(msg)
             if charset:
@@ -94,9 +99,3 @@ def print_info(msg, indent=0):
             print('%sText: %s' % ('  ' * indent, content + '...'))
         else:
             print('%sAttachment: %s' % ('  ' * indent, content_type))
-
-
-if __name__ == '__main__':
-    decode_str(s)
-    guess_charset(msg)
-    print_info(msg, indent=0)
